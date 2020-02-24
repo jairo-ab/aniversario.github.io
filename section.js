c62b5e2031e449a6af43331d1d8be95e0fbb43af
Jairo Abreu <jairo.abreu.3133@gmail.com>
2020-02-24 22:21:43 +0000

    Autocommit por GitHub.sh: alterações: add '__css/style.css' add 'index.html' add 'section.js'

---
+++ section.js
@@ -0,0 +1,9 @@
+/*$(window).on(load, function () {
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("status").style.display = "block";
+});*/
+var intervalo = setInterval(function (){
+    clearInterval(intervalo);
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("status").style.display = "block";
+},2000);
