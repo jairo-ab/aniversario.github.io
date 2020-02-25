83d8d161867df7b701cf3d23d2126acdb87d96b2
Jairo Abreu <jairo.abreu.3133@gmail.com>
2020-02-25 23:12:37 +0000

    Autocommit por GitHub.sh: alterações: add 'index.html' remove 'section.js' add '__js/section.js'

--- __js/section.js
+++ __js/section.js
@@ -1,9 +1,9 @@
 /*$(window).on('load', function () {
     document.getElementById("carregar").style.display = "none";
     document.getElementById("corpo").style.display = "block";
-});
-/*var intervalo = setInterval(function (){
+});*/
+var intervalo = setInterval(function () {
     clearInterval(intervalo);
     document.getElementById("carregar").style.display = "none";
     document.getElementById("corpo").style.display = "block";
-},3000);*/
+}, 3000)
